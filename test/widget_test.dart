4e0ab6bfab9dfd8fac1b9cfb53b932dbfcad7156
Toprak <topraktas19@hotmail.com>
2025-03-01 03:23:26 +0300

    App name changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:medium_context/main.dart';
+import 'package:medium_content/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
